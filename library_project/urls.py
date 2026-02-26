29404c809fd4beddd13d6ef02e84cb17bb90ce84
Osos <oalhendi03@gmail.com>
2026-02-26 12:49:29 +0200

    media for production

--- library_project/urls.py
+++ library_project/urls.py
@@ -9,8 +9,10 @@ urlpatterns = [
     path('', include('library.urls')),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 def error_view(request, exception=None, status=500):
